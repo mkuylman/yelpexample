839a35a3dff768d1a7e31ea036443ac066c960ac
mkuylman <mkuylman@gmail.com>
2016-02-01 16:49:06 -0800

    remove show and index urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :reviews
+  resources :reviews, except: [:show, :index]
 
   devise_for :users
-  resources :restaurants
+  resources :restaurant
 
   get 'pages/about'
 
