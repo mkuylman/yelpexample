b6acff28b39761116b613933e4692801c5627384
mkuylman <mkuylman@gmail.com>
2016-02-02 13:05:05 -0800

    Link Reviews with Restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :reviews, except: [:show, :index]
 
   devise_for :users
-  resources :restaurant
+  resources :restaurants do 
+    resources :reviews, except: [:show, :index]
+  end
 
   get 'pages/about'
 
