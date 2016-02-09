45492dd7fe0355ce30f4bde6101d9c972bf562f3
mkuylman <mkuylman@gmail.com>
2016-02-09 15:56:48 -0800

    cha cha changes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,10 +6,6 @@ class Restaurant < ActiveRecord::Base
   has_many :reviews
 
   validates :name, :address, :phone, :website, :image, presence: true
-	validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
-    message: "must be in the format (123) 456-7980" }
-  validates :website, format: { with: /\Ahttps?:\/\/.*\z\) \d{3}-\d{4}\z/,
-    message: "must start with http:// or https://" }
-  validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
-    message: "must be in the format 350 Fifth Avenue, New York, NY 11001 " }
+	validates :phone, format: { with: /\A\(\d{3}\)\d{3}-\d{4}\z/,
+    message: "must be in the format (123)456-7980" }
 end
